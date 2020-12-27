ae0c656365bdcdd8423b4565dd53c820f6d13b4c
focus <work@dengbbdeMacBook-Pro.local>
2020-12-27 21:06:08 +0900

    first commit

---
+++ wp-content/themes/destin-basic/library/js/theme_ver=5.2.9.js
@@ -0,0 +1,46 @@
+( function( $ ) {
+	// Responsive videos
+	var $all_videos = $( '.entry-content' ).find( 'iframe[src*="player.vimeo.com"], iframe[src*="youtube.com"], iframe[src*="youtube-nocookie.com"], iframe[src*="dailymotion.com"],iframe[src*="kickstarter.com"][src*="video.html"], object, embed' ),
+		$container = $( '.sticky-container' );
+
+	$all_videos.not( 'object object' ).each( function() {
+		var $video = $(this);
+
+		if ( $video.parents( 'object' ).length )
+			return;
+
+		if ( ! $video.prop( 'id' ) )
+			$video.attr( 'id', 'rvw' + Math.floor( Math.random() * 999999 ) );
+
+		$video
+			.wrap( '<div class="responsive-video-wrapper" style="padding-top: ' + ( $video.attr( 'height' ) / $video.attr( 'width' ) * 100 ) + '%" />' )
+			.removeAttr( 'height' )
+			.removeAttr( 'width' );
+	} );
+
+	$( 'a[href="#"]' ).click( function(e) {
+		e.preventDefault();
+	} );
+
+	// Masonry for home page
+	if ($container.length > 0 ) {
+    $container.find('.item').show();
+    $container.imagesLoaded( function() {
+      $container.masonry({
+        itemSelector: '.item',
+        stamp: '.stamp',
+        transitionDuration: 0
+      });
+    });
+    $container.css( 'visibility', 'visible' );
+
+		$( document.body ).on( 'post-load', function () {
+			$container.imagesLoaded( function() {
+				$container.find('.item').show();
+				$container
+					.masonry( 'reloadItems' )
+					.masonry( 'layout' );
+			}).css( 'visibility', 'visible' );
+		});
+	}
+} )( jQuery );
